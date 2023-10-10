13e98ce33cfb7d217e33b35b205771072fa4f182
alephys <alephys@sivanode1.infra.alephys.com>
2023-10-10 02:58:00 -0400

    my first commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  count = 2
+  ami           = "ami-03a6eaae9938c858c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraformtest${count.index}"
+  }
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-alephys-siva"
+
+}
+
+
